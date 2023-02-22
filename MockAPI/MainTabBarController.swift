49a95425f86a815b97597204018f2185737e4640
Jameel Shehadeh <>
2023-02-22 20:58:57 +0400

    Add TabBarControllerAppearance Add TableView in UsersViewController

--- MockAPI/MainTabBarController.swift
+++ MockAPI/MainTabBarController.swift
@@ -17,12 +17,37 @@ class MainTabBarController : UITabBarController {
     func prepareViewControllers(){
         
         let usersVC = UsersViewController()
+        
         usersVC.tabBarItem = UITabBarItem(title: "users", image: UIImage(systemName: "person"), tag: 0)
+        let usersNavVC = UINavigationController(rootViewController: usersVC)
+        
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = UIColor.systemBackground
+        
+        let titleAttribute = [NSAttributedString.Key.foregroundColor: UIColor.label]
+        navBarAppearance.titleTextAttributes = titleAttribute
+                
+        usersNavVC.navigationBar.standardAppearance = navBarAppearance
+        usersNavVC.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
+        
+        let tabBarApperance = UITabBarAppearance()
+        
+        tabBarApperance.configureWithOpaqueBackground()
+        tabBarApperance.backgroundColor = UIColor.systemBackground
+        
+        self.tabBar.tintColor = .label
+        self.tabBar.standardAppearance = tabBarApperance
+        self.tabBar.scrollEdgeAppearance = tabBarApperance
+        
         
         let postsVC = PostsViewController()
         postsVC.tabBarItem = UITabBarItem(title: "posts", image: UIImage(systemName: "square.fill.text.grid.1x2"), tag: 1)
         
-        self.addChild(usersVC)
+        self.addChild(usersNavVC)
         self.addChild(postsVC)
         
     }
--- MockAPI/UsersViewController.swift
+++ MockAPI/UsersViewController.swift
@@ -9,9 +9,51 @@ import UIKit
 
 class UsersViewController: UIViewController {
 
+    private lazy var tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.estimatedRowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI(){
+        navigationItem.title = "Users"
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor)
+            ])
     }
 
+}
+
+extension UsersViewController : UITableViewDelegate , UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    
 }
 
